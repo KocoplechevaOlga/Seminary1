28b2f3b3f19f881d946d3630b35447a9d78d9bb7
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-13 12:29:28 +0300

    Решили задачу 4

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ВВедите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = -number;
+
+while (count <= number)
+{
+    Console.Write($"{count }");
+    count++;
+}
