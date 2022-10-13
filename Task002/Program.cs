83b1d6df13d10dff8443f6aa84d7c75e2529aa15
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-13 11:48:08 +0300

    Задача 2 выполнена

---
+++ Task002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int souare = numberB * numberB;
+
+if (numberA == souare)
+{
+ Console.WriteLine($"Число a = {numberA} явлется квадратом {souare} числа b = {numberB} ");   
+}
+else 
+{
+    Console.WriteLine($"Число a {numberA} НЕ явлется квадратом числа b = {numberB} ");   
+}
