c7bc1b091750df45bce02e0c9aeac90ead9d8f29
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-13 12:44:51 +0300

    Решили задачу 5

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. принимает на вход трёхзначное число
+// 2. выходе показывает последнюю цифру этого числа.
+
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+int lastDigit = number % 10;
+Console.WriteLine($"Последняя цифра числа = {lastDigit}");
+}
+else Console.WriteLine("Вы ввели не трёхзначное число!");
